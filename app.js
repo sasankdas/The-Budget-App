91aa4a5622519e47e244bd2a528a34c61d4e9860
sasankdas <alladisasankdas@gmail.com>
2018-04-27 18:00:41 -0400

    added 3 different modules for Budget, controlling and UI (IIFE).

---
+++ app.js
@@ -0,0 +1,29 @@
+var budgetController = (function(){
+
+
+
+    return {
+
+    }
+})();
+
+var UIController = (function(){
+
+
+
+    return 
+
+
+
+})();
+
+
+var controller = (function(budgetCtrl, UICtrl){
+
+
+
+    return 
+
+
+
+}) (budgetController, UIController);
