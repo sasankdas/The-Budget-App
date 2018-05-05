2d35ec42291096ba5f36e5b5c4d803b6ee5687a4
sasankdas <alladisasankdas@gmail.com>
2018-05-05 17:59:33 -0400

    Added and updating UI dynamically

--- app.js
+++ app.js
@@ -81,6 +81,7 @@ return {
 
                     },
 
+
                     getBudget: function(){
                        return{
                            budget:data.budget,
@@ -103,7 +104,11 @@ var DOMstrings = {
     inputValue : ".add__value",
     inputBtn : ".add__btn",
     incomeContainer: ".income__list",
-    expensesContainer: ".expenses__list"
+    expensesContainer: ".expenses__list",
+    budgetLabel: ".budget__value",
+    incomeLabel: ".budget__income--value",
+    expensesLabel: ".budget__expenses--value",
+    percentageLabel: ".budget__expenses--percentage",
 }
 return{
 
@@ -141,6 +146,12 @@ return{
             fieldsArr[0].focus();
 
         },
+        displayBudget : function(obj){
+document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+        },
 
     getDOMstrings: function(){
         return DOMstrings;
@@ -170,8 +181,6 @@ var setupEventListeners = function (){
 
 
 
-
-
 var updateBudget= function (){
     //calculate budget
 budgetCtrl.calculateBudget();
@@ -180,6 +189,9 @@ budgetCtrl.calculateBudget();
     var budget= budgetCtrl.getBudget();
 
     //display budget on ui
+
+
+    UICtrl.displayBudget(budget);
 
     console.log(budget);
 }
